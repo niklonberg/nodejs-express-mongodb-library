01c396427febcf217b4e59c30b950c4fb573ed0b
Nikolai <niklonberg@gmail.com>
2024-05-19 10:54:13 +0200

    set express app view folder directory

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require("./routes/index");
 
 app.set("view engine", "ejs"); // set view engine to use ejs
-app.set("views", path.resolve(__dirname, "/views")); // set where our views will come from
+app.set("views", path.resolve(__dirname, "views")); // set where our views will come from
 app.set("layout", "layouts/layout"); // every single view will be 'put inside' this layout file, so things like our header / footer dont need to be duplicated for each view
 app.use(expressLayouts); // ensure express uses express layouts
 app.use(express.static("public")); // set express to use public files located in public folder
