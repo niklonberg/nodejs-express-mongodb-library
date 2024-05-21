529c17adfea16d7e24a6f68f8441379469ce3290
Nikolai <niklonberg@gmail.com>
2024-05-21 09:28:21 +0200

    add authors controller/routes

---
+++ routes/authors.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+// All authors route
+router.get("/", (req, res) => {
+  res.render("authors/index");
+});
+
+// New author route
+router.get("/new", (req, res) => {
+  res.render("authors/new");
+});
+
+// Create Author route
+router.post("/", (req, res) => {
+  res.send("Create");
+});
+
+module.exports = router;
