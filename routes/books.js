aff752125c65d085318902dd586a551c130e6cdc
Nikolai <niklonberg@gmail.com>
2024-05-21 13:19:52 +0200

    add book route links

--- routes/books.js
+++ routes/books.js
@@ -3,14 +3,18 @@ const Book = require("../models/book");
 const router = express.Router();
 
 // All books route
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  res.send("All books");
+});
 
 // New book route
 router.get("/new", (req, res) => {
-  
+  res.send("New books");
 });
 
 // Create book route
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  res.send("create books");
+});
 
 module.exports = router;
