7bb553b275d8c1630ebc423968af1ffbebab2385
Nikolai <niklonberg@gmail.com>
2024-05-21 11:25:14 +0200

    edit get all authors route

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,15 @@ const Author = require("../models/author");
 const router = express.Router();
 
 // All authors route
-router.get("/", (req, res) => {
-  res.render("authors/index");
+router.get("/", async (req, res) => {
+  try {
+    // get all authors
+    const authors = await Author.find({});
+    // render authors/index, passing authors to the template
+    res.render("authors/index", { authors });
+  } catch (error) {
+    res.redirect("/");
+  }
 });
 
 // New author route
@@ -22,7 +29,8 @@ router.post("/", async (req, res) => {
     // save new author to database
     const newAuthor = await author.save();
     // if success redirect to newly created author
-    res.redirect(`author/${newAuthor.id}`);
+    // res.redirect(`author/${newAuthor.id}`);
+    res.redirect("authors");
   } catch (error) {
     // if error occurs, render authors/new and auto populate the fields with what the user entered
     res.render("authors/new", {
