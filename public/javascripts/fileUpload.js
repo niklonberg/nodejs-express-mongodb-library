5ad7498d40b80513e06781e2db235a7c67de37ea
Nikolai <niklonberg@gmail.com>
2024-05-22 12:49:03 +0200

    ensure img files uploaded are resized to fit our aspect ratio

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -5,7 +5,9 @@ FilePond.registerPlugin(
 );
 
 FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100
+  stylePanelAspectRatio: 150 / 100,
+  imageResizeTargetHeight: 150,
+  imageResizeTargetWidth: 100,  
 })
 
 FilePond.parse(document.body);
