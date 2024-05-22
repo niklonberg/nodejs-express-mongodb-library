966b1e522c4470c15f6f226b822f71fac65e0cdf
Nikolai <niklonberg@gmail.com>
2024-05-22 15:54:24 +0200

    add mongoose middleware function to run before attempting to delete an author

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Book = require("./book");
 
 // Create author schema - equivalent to a table in sql db
 const authorSchema = new mongoose.Schema({
@@ -6,6 +7,21 @@ const authorSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+// add function to run before a database 'remove' (delete) attempt
+authorSchema.pre("findOneAndDelete", function (next) {
+  Book.find({ author: this.id }, (err, books) => {
+    if (err) {
+      next(err);
+    } else if (books.length > 0) {
+      // prevent author deletion if there are books with that author tied to it
+      next(new Error("This author has books tied to them"));
+    } else {
+      // proceed with deletion
+      next();
+    }
+  });
 });
 
 // export newly created model, with name Author and specified schema for it
