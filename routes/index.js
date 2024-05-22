b3cce65fdf4833519037c3e2f55b15ef8033c2de
Nikolai <niklonberg@gmail.com>
2024-05-22 11:33:20 +0200

    edit homepage to render last 10 books

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const Book = require("../models/book");
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  let books;
+  try {
+    // get last 10 books added
+    books = await Book.find().sort({ createdAt: "desc" }).limit(10).exec();
+    res.render("index", { books });
+  } catch (error) {
+    // in case of server error, set books to empty array so template can still render
+    books = [];
+  }
 });
 
 module.exports = router;
