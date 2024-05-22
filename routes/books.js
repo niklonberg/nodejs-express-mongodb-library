e21d936333324490b06dddbcd77148e3575c532c
Nikolai <niklonberg@gmail.com>
2024-05-22 13:20:53 +0200

    remove unused modules

--- routes/books.js
+++ routes/books.js
@@ -1,46 +1,33 @@
 const express = require("express");
 const Book = require("../models/book");
 const Author = require("../models/author");
-const path = require("path");
-const fs = require("fs");
 const router = express.Router();
-
-// create book cover upload path <-- see book model
-const uploadPath = path.join("public", Book.coverImageBasePath);
 // create imageMimeTypes, which is an array of types of image files we'll accept
 const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
-// use multer library
-// const coverImageFileUpload = multer({
-//   dest: uploadPath, // destination path
-//   fileFilter: (req, file, callback) => {
-//     // a filter method
-//     callback(null, imageMimeTypes.includes(file.mimetype));
-//   },
-// });
 
 // All books route
 router.get("/", async (req, res) => {
-  let query = Book.find()
-  if (req.query.title != null && req.query.title !== '') 
+  let query = Book.find();
+  if (req.query.title != null && req.query.title !== "")
     // case-insensitive search for title
-    query = query.regex('title', new RegExp(req.query.title, 'i'))
-  
-  if (req.query.publishedBefore != null && req.query.publishedBefore !== '')
+    query = query.regex("title", new RegExp(req.query.title, "i"));
+
+  if (req.query.publishedBefore != null && req.query.publishedBefore !== "")
     // check that publishedBefore date is less than the books publishDate
-    query = query.lte('publishDate', req.query.publishedBefore)
-  
-  if (req.query.publishedAfter != null && req.query.publishedAfter !== '') 
+    query = query.lte("publishDate", req.query.publishedBefore);
+
+  if (req.query.publishedAfter != null && req.query.publishedAfter !== "")
     // check for greater than
-    query = query.gte('publishDate', req.query.publishedAfter)
-  
+    query = query.gte("publishDate", req.query.publishedAfter);
+
   try {
-    const books = await query.exec()
+    const books = await query.exec();
     res.render("books/index", {
       books,
-      searchOptions: req.query
+      searchOptions: req.query,
     });
   } catch (error) {
-    res.redirect('/')
+    res.redirect("/");
   }
 });
 
@@ -58,7 +45,7 @@ router.post("/", async (req, res) => {
     pageCount: req.body.pageCount,
     description: req.body.description,
   });
-  saveCover(book, req.body.cover)
+  saveCover(book, req.body.cover);
 
   try {
     const newBook = await book.save();
@@ -84,11 +71,11 @@ async function renderNewPage(res, book, hasError = false) {
 }
 
 function saveCover(book, coverEncoded) {
-  if (coverEncoded == null) return
-  const cover = JSON.parse(coverEncoded)
+  if (coverEncoded == null) return;
+  const cover = JSON.parse(coverEncoded);
   if (cover != null && imageMimeTypes.includes(cover.type)) {
-    book.coverImage = new Buffer.from(cover.data, 'base64')
-    book.coverImageType = cover.type
+    book.coverImage = new Buffer.from(cover.data, "base64");
+    book.coverImageType = cover.type;
   }
 }
 
